a4a23df186372aa07d7553e40662e995c8856f63
Rui <lirui.jobs@gmail.com>
2019-12-31 22:35:02 +1100

    Updates

---
+++ precache-manifest.ff28cf63a2a33ca7e48a928c634f7536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd659b7bd36011afce6487a1a662f9b",
+    "url": "/DailyExpenseReact/index.html"
+  },
+  {
+    "revision": "25995264cc486ebf13f7",
+    "url": "/DailyExpenseReact/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "4223d61cf5aad2865a8d",
+    "url": "/DailyExpenseReact/static/css/main.683d5d11.chunk.css"
+  },
+  {
+    "revision": "25995264cc486ebf13f7",
+    "url": "/DailyExpenseReact/static/js/2.05e1f07e.chunk.js"
+  },
+  {
+    "revision": "4223d61cf5aad2865a8d",
+    "url": "/DailyExpenseReact/static/js/main.d15ab7e0.chunk.js"
+  },
+  {
+    "revision": "29a69a96d62c92b824da",
+    "url": "/DailyExpenseReact/static/js/runtime-main.8edf72b3.js"
+  }
+]);
